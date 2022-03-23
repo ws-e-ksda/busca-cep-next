9ac337fd1dccaaebc33717ff67e100539ae6139f
ws-ksda <kevin.andrade@wilsonsons.com.br>
2022-03-23 09:11:31 -0300

    Passando Busca CEP para Nextjs

---
+++ components/Cep.tsx
@@ -0,0 +1,7 @@
+export function Cep(props: any){
+    return (
+        <div>
+            {props.children}
+        </div>
+    )
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,12 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+
+// Prime Themes
+import 'primeicons/primeicons.css';
+import 'primereact/resources/themes/lara-dark-purple/theme.css';
+import 'primereact/resources/primereact.css';
+import 'primeflex/primeflex.css';
+import '/node_modules/primeflex/primeflex.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
