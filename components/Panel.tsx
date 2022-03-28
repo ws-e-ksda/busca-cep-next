0617a45a95a8b2d4f2b8ebcca23fea39b66cfce9
ws-ksda <kevin.andrade@wilsonsons.com.br>
2022-03-28 11:59:00 -0300

    Trocando ícone e título do app

--- components/Panel.tsx
+++ components/Panel.tsx
@@ -4,20 +4,27 @@ import { Button } from 'primereact/button';
 
 // Next
 import Link from 'next/link';
+import Head from 'next/head';
 
 export const Panel = (props: any) =>{
     return (
-        <Card className="card shadow-5 border-50 border-round border-1 m-3">
-            <Link href="/">
-                <Button className="p-button-outlined p-button-sm"
-                        icon="pi pi-arrow-left" >
-                    <a></a>
-                </Button>
-            </Link>
-            <h1 className='text-primary'>{props.title}</h1>
-            {/* Forms */}
-            {props.children}
-            
-        </Card>
+        <>
+            <Head>
+            <title>Busca CEP</title>
+            <link rel="icon" href="/brasil.ico" />
+            </Head>
+            <Card className="card shadow-5 border-50 border-round border-1 m-3">
+                <Link href="/">
+                    <Button className="p-button-outlined p-button-sm"
+                            icon="pi pi-arrow-left" >
+                        <a></a>
+                    </Button>
+                </Link>
+                <h1 className='text-primary'>{props.title}</h1>
+                {/* Forms */}
+                {props.children}
+                
+            </Card>
+        </>
     );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 // Next.js
 import type { NextPage } from 'next';
+import Head              from 'next/head';
 import Link              from 'next/link';
 
 // Primereact
@@ -10,23 +11,27 @@ const Home: NextPage = () => {
   
     return(
       <>
-          <Card className="card shadow-5 border-50 border-round border-1 m-3">
-            <h1 className="text-primary flex justify-content-center">O que você deseja encontrar?</h1>
+        <Head>
+          <title>Busca CEP</title>
+          <link rel="icon" href="/brasil.ico" />
+        </Head>
+        <Card className="card shadow-5 border-50 border-round border-1 m-3">
+          <h1 className="text-primary flex justify-content-center">O que você deseja encontrar?</h1>
+          
+          <div id="links" className='flex justify-content-center align-content-center p-3'>
+            <Link href="buscar-cep">
+              <Button className='p-button-link p-button-raised mr-2'>
+                <a >Buscar CEP</a>
+              </Button>
+            </Link>
             
-            <div id="links" className='flex justify-content-center align-content-center p-3'>
-              <Link href="buscar-cep">
-                <Button className='p-button-link p-button-raised mr-2'>
-                  <a >Buscar CEP</a>
-                </Button>
-              </Link>
-              
-              <Link href="buscar-endereco">
-                <Button className='p-button-link p-button-raised ml-2'>
-                  <a >Buscar endereço</a>
-                </Button>
-              </Link>
-            </div>
-          </Card>
+            <Link href="buscar-endereco">
+              <Button className='p-button-link p-button-raised ml-2'>
+                <a >Buscar endereço</a>
+              </Button>
+            </Link>
+          </div>
+        </Card>
       </>
     );
 
