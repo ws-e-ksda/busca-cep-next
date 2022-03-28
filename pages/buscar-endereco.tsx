7e2ffac64685e212fe14ff6ecc4b3ccfaca5f1b1
ws-ksda <kevin.andrade@wilsonsons.com.br>
2022-03-28 10:54:29 -0300

    Buscar endereço a partir do CEP

--- pages/buscar-endereco.tsx
+++ pages/buscar-endereco.tsx
@@ -2,8 +2,7 @@
 import { useState, useRef } from 'react';
 
 // Primereact
-import { InputText } from 'primereact/inputtext';
-import { Dropdown }   from 'primereact/dropdown';
+import { InputMask } from "primereact/inputmask";
 import { Button }    from 'primereact/button';
 import { Toast }     from 'primereact/toast';
 
@@ -18,28 +17,26 @@ import { InfoStatus } from '../models/infoStatus';
 // Serviço
 import CepService from '../services/search-cep-service';
 
+// ------------------------------------------------------- \\
+
 export default function BuscarEndereco(){
 
-    const ufs = ['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 
-                'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 
-                'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS',
-                'RO', 'RR', 'SC', 'SP', 'SE', 'TO'];
+    const [cep, setCep] = useState(""); // CEP input
+    const onChangeCep = (e: any) => setCep(e.target.value);
 
-    const [selectedUF, setSelectedUF] = useState("");
-
-    const [info, setInfo] = useState<Array<Data>>([]); // Infos da tabela
-    const [historic, setHistoric] = useState(new Map()); // Histórico de ceps pesquisados
-    
     const [buttonLoading, setButtonLoading] = useState(false); // Button submit
     const [buttonCheckIcon, setButtonCheckIcon] = useState("pi pi-check");
 
+    const [info, setInfo] = useState<Array<Data>>([]); // Infos da tabela
+    const [historic, setHistoric] = useState(new Map()); // Histórico de ceps pesquisados
 
     const toast: any = useRef(null);
-    
-    const handleSubmitForm = (e: any) => {
+
+    const handleSubmitForm = async(e: any) => {
         e.preventDefault();
-        // setButtonLoading(true);
-        // setButtonCheckIcon("");
+  
+        setButtonLoading(true);
+        setButtonCheckIcon("");
         
         // A princípio a requisição deu certo
         let toastMessage = {
@@ -47,45 +44,99 @@ export default function BuscarEndereco(){
             summary:  "Requisição concluída", 
             detail:   "CEP buscado com sucesso"
         }
+
+        const cepWithoutMask = cep.replace("-", "").replace(".", "");
+
+        // Verifica se está no mapa
+        let infoStatus: InfoStatus = historic.get(cepWithoutMask);
+        if(infoStatus){
+            if(infoStatus.statusErro){
+                toastMessage = {
+                    severity: "warn",
+                    summary: "Erro na requisição",
+                    detail: "CEP consultado não foi encontrado na base de dados"
+                }
+            }
+        }else{ // Se não está no mapa, faz nova requisição
+            const cepService = new CepService();
+            const response = await cepService.getAdress(cepWithoutMask);
+
+            // Tratando erro 502
+            if(response.badRequest){
+                toastMessage = {
+                    severity: "error",
+                    summary: "Erro na requisição",
+                    detail: "Falha ao tentar buscar CEP"
+                }
+
+                infoStatus = {
+                    info: [],
+                    statusErro: true
+                };
+            }else{
+                if(response.erro){
+                    infoStatus = {
+                        info: [],
+                        statusErro: true
+                    };
+    
+                    toastMessage = {
+                        severity: "warn",
+                        summary: "Erro na requisição",
+                        detail: "CEP consultado não foi encontrado na base de dados"
+                    }
+                }else{
+                    infoStatus = {
+                        info: [response],
+                        statusErro: false
+                    };
+                }
+                // Atualiza o mapa
+                const _historic = new Map(historic);
+                _historic.set(cepWithoutMask, infoStatus);
+                setHistoric(_historic);
+            }
+        }   
+
+        setInfo(infoStatus.info);
+
+        setButtonLoading(false);
+        setButtonCheckIcon("pi pi-check");
+
+        // Mensagem de feedback
+        toast.current.show({...toastMessage});
     }
 
-    return(
+    return (
         <Panel title="Buscar endereço">
-            <form onSubmit={handleSubmitForm}>
-                <div  className="inline-flex">
-                    <div className="field">
-                        <label htmlFor="uf" className="block" >UF</label>
-                        <Dropdown className="mr-3" id="uf" placeholder="Ex.: RJ"
-                                value={selectedUF}
-                                options={ufs}
-                                required
-                                onChange={(e) => setSelectedUF(e.target.value)}/>
-                    </div>
-
-                    <div className="field">
-                        <label htmlFor="cidade" className="block">Cidade</label>
-                        <InputText className="mr-3" id="cidade" placeholder="Ex.: Rio de Janeiro" 
-                                required
-                                minLength={3}/>
-                    </div>
+            <form onSubmit={handleSubmitForm} >
+                <div className='sm:inline-flex'> 
                     <div className='field'>
-                        <label htmlFor="logradouro" className="block">Logradouro</label>
-                        <InputText className="mr-3" id="logradouro" placeholder="Ex.: Rua do Rocha" 
+                        <label htmlFor='cep' className='block'>CEP</label>
+                        <span className = "p-input-icon-left block w-10rem">    
+                            <i className = "pi pi-search"/>
+                            <InputMask className='p-inputtext inputfield w-full'
+                                id = "cep"
+                                value={cep}
+                                placeholder = "99.999-999"
                                 required
-                                minLength={3}/>
+                                mask='99.999-999'
+                                onChange={onChangeCep}
+                            />
+                        </span>
                     </div>
                     <div className='flex align-content-end flex-wrap'>
-                        <Button className = "p-button align-self-center mb-4"
+                        <Button className = "p-button  align-self-center mb-4 mt-3 block sm:ml-3 sm:mt-0"
                             label = 'Enviar'
                             icon  = {buttonCheckIcon}
                             loading = {buttonLoading}
                             type='submit'
                         />
-                    </div>                  
+                    </div>
                 </div>
             </form>
             <DataTableCep value = {info}/>
             <Toast ref={toast} className="primary"/>
-        </Panel>
+        </Panel>            
     );
 }
