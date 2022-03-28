90cb43a6f5c30f4cd94a5d8a7ed983cea4f13f23
ws-ksda <kevin.andrade@wilsonsons.com.br>
2022-03-28 12:00:32 -0300

    Novo método de consulta à API adicionado (getCep)

--- services/search-cep-service.tsx
+++ services/search-cep-service.tsx
@@ -1,21 +1,46 @@
+// Axios
 import axios, { AxiosRequestConfig } from 'axios';
 
+// Models
+import { DataAdress } from '../models/data-endereco';
+
 class CepService{
+    prefix: string = "https://viacep.com.br/ws";
     async getAdress(cep:string) {
 
-        const url = `https://viacep.com.br/ws/${cep}/json`;
+        const url = `${this.prefix}/${cep}/json`;
         let options: AxiosRequestConfig = {
             method: 'get',
             url: url
         };
 
-        let response = await axios(options);
-        return response.data;
+        let response:any = {};
+        await axios(options)
+        .then((res) => {
+            response = res.data;
+        })
+        .catch(() => {response.badRequest = true});
+
+        return response;
     }
 
-//     async function getCep(params:) {
+    async getCep(params: DataAdress) {
+
+        const url =  `${this.prefix}/${params.uf}/${params.city}/${params.publicPlace}/json`;
+        let options: AxiosRequestConfig = {
+            method: 'get',
+            url: url
+        }
         
-//     }
+        let response:any = {};
+        await axios(options)
+        .then((res) => {
+            response = res.data;
+        })
+        .catch(() => {response.badRequest = true});
+
+        return response;
+    }
 }
 
 export default CepService;
