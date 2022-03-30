feaa8bf4c80436807d58b407f41661930bbc3169
ws-ksda <kevin.andrade@wilsonsons.com.br>
2022-03-30 12:59:37 -0300

    Tratando bug responsividade

--- components/DataTableCep.tsx
+++ components/DataTableCep.tsx
@@ -4,35 +4,39 @@ import { Column } from 'primereact/column';
 
 // Models
 import { Data } from '../models/data';
+import { useEffect, useState } from 'react';
+import { Button } from 'primereact/button';
 
-interface DataTableCepProps{
+interface DataTableCepProps {
     value: Data[];
     sortable?: boolean;
     paginator?: boolean;
 }
 
-export const DataTableCep = ({value, sortable, paginator}: DataTableCepProps) =>{
-    return(
+export const DataTableCep = ({ value, sortable, paginator }: DataTableCepProps) => {
+
+    return (
         <>
-            <DataTable 
-                value={value} 
-                responsiveLayout = "stack"
+            <DataTable
+                breakpoint='1280px'
+                value={value}
+                responsiveLayout="stack"
                 rows={5}
                 rowsPerPageOptions={[5, 10, 25]}
                 paginator={paginator}
                 paginatorTemplate={"RowsPerPageDropdown CurrentPageReport PrevPageLink NextPageLink"}
                 paginatorClassName="justify-content-end"
-                emptyMessage = "Nenhuma informação encontrada.">
-                <Column field = "cep" header = "CEP" sortable={sortable}/>
-                <Column field = "logradouro" header = "Logradouro" sortable={sortable}/>
-                <Column field = "complemento" header = "Complemento" sortable={sortable}/>
-                <Column field = "bairro" header = "Bairro" sortable={sortable}/>
-                <Column field = "localidade" header = "Localidade" sortable={sortable}/>
-                <Column field = "uf" header = "UF" sortable={sortable}/>
-                <Column field = "ibge" header = "IBGE" sortable={sortable}/>
-                <Column field = "gia" header = "GIA" sortable={sortable}/>
-                <Column field = "ddd" header = "DDD" sortable={sortable}/>
-                <Column field = "siafi" header = "SIAFI" sortable={sortable}/>
+                emptyMessage="Nenhuma informação encontrada.">
+                <Column field="cep" header="CEP" sortable={sortable} />
+                <Column field="logradouro" header="Logradouro" sortable={sortable} />
+                <Column field="complemento" header="Complemento" sortable={sortable} />
+                <Column field="bairro" header="Bairro" sortable={sortable} />
+                <Column field="localidade" header="Localidade" sortable={sortable} />
+                <Column field="uf" header="UF" sortable={sortable} />
+                <Column field="ibge" header="IBGE" sortable={sortable} />
+                <Column field="gia" header="GIA" sortable={sortable} />
+                <Column field="ddd" header="DDD" sortable={sortable} />
+                <Column field="siafi" header="SIAFI" sortable={sortable} />
             </DataTable>
         </>
     );
